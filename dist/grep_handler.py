1acddee7fd05c4a3ea87689de1b6b25e39f6a692
scottnuma <scott.numamoto@gmail.com>
2019-03-11 20:24:39 +0000

    Add grep demo application to dist
    - Copy in one version of sfs.py

---
+++ dist/grep_handler.py
@@ -0,0 +1,24 @@
+import os
+import re
+import sys
+
+from sfs import *
+
+def grep(file_name, phrase):
+    p = re.compile(phrase)
+    matched_lines = []
+    with open(file_name, mode='r') as f:
+        for line in f:
+            if p.search(line):
+                matched_lines.append(file_name + ": " + line)
+    return matched_lines
+
+
+def lambda_handler(event, context):
+    mount_point = os.environ["NFS4_SERVER"]
+    mount(mount_point)
+    print("event", event)
+    return {
+        "matches": grep(event["file_path"], event["phrase"]),
+        "file_name": event["file_path"],
+    }
